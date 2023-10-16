ad878982c4929752102d581d7d746cc759705636
Rachel Flood Heaton <54960469+rachelfheaton@users.noreply.github.com>
2023-10-15 22:16:13 -0500

    Add files via upload

--- GraphicalRun1.py
+++ GraphicalRun1.py
@@ -1,3 +1,22 @@
+#* Copyright 2023 The Board of Trustees of the University of Illinois. All Rights Reserved.
+#
+#* Licensed under the terms of the Apache License 2.0 license (the "License")
+#
+#* The License is included in the distribution as License.txt file.
+#
+#* You may not use this file except in compliance with the License.
+#
+#* Software distributed under the License is distributed on an "AS IS" BASIS,
+#
+#* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+#* See the License for the specific language governing permissions and limitations under the Option.
+
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+# CASPER MODEL OF VISUAL SEARCH
+# (Concurrent Attention: Serial and Parallel Evaluation with Relations)
+# Developed and written by Rachel F Heaton and John E Hummel
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 # graphical run for the attention model, version 1      2/4/19
 
