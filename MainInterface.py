56bbb5bccd6d8ddebf4d2b1cc7eaac9919e917d3
simonabuetti <149421232+simonabuetti@users.noreply.github.com>
2023-11-03 16:55:00 -0500

    Update MainInterface.py

--- MainInterface.py
+++ MainInterface.py
@@ -1,24 +1,3 @@
-#* Copyright 2023 The Board of Trustees of the University of Illinois. All Rights Reserved.
-#
-#* Licensed under the terms of the Apache License 2.0 license (the "License")
-#
-#* The License is included in the distribution as License.txt file.
-#
-#* You may not use this file except in compliance with the License.
-#
-#* Software distributed under the License is distributed on an "AS IS" BASIS,
-#
-#* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#
-#* See the License for the specific language governing permissions and limitations under the Option.
-
-# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-# CASPER MODEL OF VISUAL SEARCH
-# (Concurrent Attention: Serial and Parallel Evaluation with Relations)
-# Developed and written by Rachel F Heaton and John E Hummel
-# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-
-
 
 import sys, SearchModel1, GraphicalRun1, copy, csv
 
